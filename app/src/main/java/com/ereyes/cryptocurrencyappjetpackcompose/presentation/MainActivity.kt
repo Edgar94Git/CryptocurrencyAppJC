8edbde1f51e07d98612d2a635764c3971da9daa7
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2023-04-18 19:16:00 -0600

    cambios pedientes

--- app/src/main/java/com/ereyes/cryptocurrencyappjetpackcompose/presentation/MainActivity.kt
+++ app/src/main/java/com/ereyes/cryptocurrencyappjetpackcompose/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ereyes.cryptocurrencyappjetpackcompose
+package com.ereyes.cryptocurrencyappjetpackcompose.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.ereyes.cryptocurrencyappjetpackcompose.ui.theme.CryptocurrencyAppJCTheme
+import com.ereyes.cryptocurrencyappjetpackcompose.presentation.ui.theme.CryptocurrencyAppJCTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
