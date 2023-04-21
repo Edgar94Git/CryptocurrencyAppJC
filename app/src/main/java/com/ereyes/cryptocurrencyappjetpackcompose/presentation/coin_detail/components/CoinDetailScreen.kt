3077f7c3e1679bbcfaecf40daaab40f35bbb94de
Edgar Reyes <60406916+Edgar94Git@users.noreply.github.com>
2023-04-21 08:14:14 -0600

    create component CoinDetailScreen

---
+++ app/src/main/java/com/ereyes/cryptocurrencyappjetpackcompose/presentation/coin_detail/components/CoinDetailScreen.kt
@@ -0,0 +1,129 @@
+package com.ereyes.cryptocurrencyappjetpackcompose.presentation.coin_detail.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScopeInstance.align
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.ereyes.cryptocurrencyappjetpackcompose.presentation.coin_detail.CoinDetailViewModel
+import com.google.accompanist.flowlayout.FlowRow
+
+/****
+ * Project: CryptocurrencyAppJC
+ * From: com.ereyes.cryptocurrencyappjetpackcompose.presentation.coin_detail.components
+ * Created by Edgar Reyes Gonzalez on 4/21/2023 at 7:18 AM
+ * All rights reserved 2023.
+ ****/
+
+@Composable
+fun CoinDetailScreen(
+    viewModel: CoinDetailViewModel = hiltViewModel()
+){
+    val state = viewModel.state().value
+
+    Box(modifier = Modifier.fillMaxWidth()){
+        state.coin?.let { coin ->
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = PaddingValues(20.dp)
+            ){
+                item {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "${coin.rank}. ${coin.name} (${coin.symbol})",
+                            fontWeight = FontWeight.SemiBold,
+                            fontSize = 24.sp,
+                            modifier = Modifier.weight(8f)
+                        )
+                        Text(
+                            text = if(coin.isActive) "active" else "inactive",
+                            color = if(coin.isActive) Color.Green else Color.Red,
+                            fontStyle = FontStyle.Italic,
+                            textAlign = TextAlign.End,
+                            modifier = Modifier
+                                .align(CenterVertically)
+                                .weight(2f)
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Text(
+                        text = coin.description,
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 14.sp
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Text(
+                        text = "Tags",
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 20.sp
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                    FlowRow(
+                        mainAxisSpacing = 10.dp,
+                        crossAxisSpacing = 10.dp,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        coin.tags.forEach { tag ->
+                            CoinTag(tag = tag)
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Text(
+                        text = "Team members",
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 20.sp
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                }
+                items(coin.team){teamMember ->
+                    TeamListItem(
+                        teamMember = teamMember,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                    )
+                    Divider()
+                }
+            }
+        }
+        if(state.error.isNotBlank()){
+            Text(
+                text = state.error,
+                color = MaterialTheme.colorScheme.error,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .align(Alignment.Center)
+            )
+        }
+        if(state.isLoading){
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    }
+
+}
